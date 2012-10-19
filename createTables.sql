4a22c55f695ce2706b91c77307519851c0472a2a
Cathy Fisher <cathy.a.fisher@gmail.com>
2012-10-19 13:01:26 -0400

    SQL files

---
+++ createTables.sql
@@ -0,0 +1,44 @@
+use itaps;
+
+CREATE TABLE Class(
+class_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(128) NOT NULL,
+link VARCHAR(2000),
+credits INT(2) NOT NULL,
+pep_credits INT(2),
+UNIQUE (title) );
+
+CREATE TABLE Student(
+user_id INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(128) NOT NULL,
+password VARCHAR(128) NOT NULL,
+specialization VARCHAR(128) NOT NULL,
+year INT(4),
+UNIQUE(username) );
+
+CREATE TABLE Manually_Entered_Class(
+class_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(128) NOT NULL,
+credits INT(2) NOT NULL,
+pep_credits INT(2),
+UNIQUE (title) );
+
+ALTER TABLE Manually_Entered_Class
+AUTO_INCREMENT = 1000;
+
+CREATE TABLE Requirements(
+r_id INT AUTO_INCREMENT PRIMARY KEY,
+specialization VARCHAR(128) NOT NULL,
+description VARCHAR(1000) NOT NULL,
+credits INT(2),
+UNIQUE(specialization) );
+
+CREATE TABLE Takes(
+user_id INT,
+class_id INT,
+UNIQUE(user_id, class_id) );
+
+CREATE TABLE Fulfills(
+r_id INT,
+class_id INT,
+UNIQUE(r_id, class_id) );
---
+++ loadData.sql
@@ -0,0 +1,7 @@
+use itaps;
+
+INSERT INTO Class(title, link, credits, pep_credits)
+VALUES ('SI 523 - Information and Control', 'http://genericlink', 3, 0);
+
+INSERT INTO Class(title, link, credits, pep_credits)
+VALUES('SI 530 - Principles in Management', 'http://anotherlink', 3, 0);
