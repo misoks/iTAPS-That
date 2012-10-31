1c3727e474395e2af8cfce19060cbd965be17efe
chingja <chingja@umich.edu>
2012-10-30 21:29:13 -0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@ $user = $_SESSION['username'];
 //connect to db
 $db = mysql_connect("localhost","kdsoltis", "sql1user") or die('Fail message');//change to your username/password to test on your machine
 mysql_select_db("itaps") or die("Fail message");//create itaps db and run createTables, loadData before using these php files
-$get = mysql_query("SELECT * FROM `users` WHERE user_level='1' AND user_level='0'");
+$get = mysql_query("SELECT * FROM users WHERE user_level='1' AND user_level='0'");
 while($row = mysql_fetch_array($get)) 
 {
 	$admin = $row['user_level'];
