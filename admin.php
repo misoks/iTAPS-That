73c7c8a2ca3eb9f4230d41b653c21f50c0c2cc16
chingja <chingja@umich.edu>
2012-11-08 01:20:39 -0500

    Admin.php fixed so that manual links to it
    Admin page lacks functionality, I need to work on it more this weekend and figure out what we want to do with it.

--- admin.php
+++ admin.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$username = $_SESSION['username'];
 //connect to db
 require_once "db.php";
 include_once('header.php');
@@ -12,11 +11,28 @@ if(isset($_POST['class_id']) && isset($_SESSION['userid'])) {
             VALUES ('$class_id', '$userid')";
     mysql_query($sql);
     $course_title = get_title($class_id);
-	movePage('manual.php', "$course_title successfully added!", 'success');
+    movePage('manual.php', "$course_title successfully added!", 'success');
+    return;
+}
+
+
+  if(isset($_POST['delete']) && $_POST['delete'] != -1) {	
+    $class_id = mysql_real_escape_string($_POST['delete']);
+    $userid = mysql_real_escape_string($_SESSION['userid']);
+ 	 	$sql = "DELETE FROM Takes  WHERE user_id = '$userid' and class_id = '$class_id'";
+    mysql_query($sql);
+ 	 	$course_title = get_title($class_id);	
+    if(mysql_affected_rows() == 1){	
+    movePage('manual.php',"$course_title successfully removed!", 'success');
+    }	
+    else{	
+    movePage('manual.php',"$course_title was not able to be removed. Please try again.", 'error');
+ 	   }	
     return;
 }
 ?>
-   
+
+
 <h1>Select a Class</h1>
 <form method="post">
 <?php
@@ -28,9 +44,8 @@ if(isset($_POST['class_id']) && isset($_SESSION['userid'])) {
 		$result = mysql_query($sql2);
 
 		while($row = mysql_fetch_row($result)){
-			echo '<input type = "radio" value ="'.htmlentities($row[0]).'" name ="class_id">'.htmlentities($row[1]).'<br>';
-		}if ($result){
-		if(mysql_num_rows($result) == 0);
+			echo '<input type = "checkbox" value ="'.htmlentities($row[0]).'" name ="class_id">'.htmlentities($row[1]).'<br>';
+		}if(mysql_num_rows($result) == 0);
 			echo "Your search returned no results.<br>";	
 		}else{
 			echo '<a href="manual.php">Go Back</a>';
@@ -39,27 +54,27 @@ if(isset($_POST['class_id']) && isset($_SESSION['userid'])) {
 <p><input type="submit" value="Add Class"/></p>
 </form>
 
+</td>
+<td id="added-classes">
+<h2>View/Remove My Classes</h2>	
+<form method="post">
+<select name= "delete">
+<option value=-1>View/Select</option>
+<?php
+$userid = $_SESSION['userid'];
+ 	$sql_myclasses = "SELECT c.class_id, c.title FROM Class c, Takes t
+  WHERE c.class_id = t.class_id and t.user_id = 
+ 	'$userid' UNION SELECT m.class_id, m.title from 
+  Manually_Entered_Class m, Takes t1 WHERE m.class_id =
+  t1.class_id and t1.user_id = '$userid'";
+  $result = mysql_query($sql_myclasses);
+  while($row = mysql_fetch_row($result)){
+    echo "<option value=".htmlentities($row[0]).">".strtrim(htmlentities($row[1]))."</option>";
+ }
+?>
+p><input type="submit" value="Delete"/></p>
+</form></td></tr></table>
 
-/* $get = mysql_query("SELECT * FROM users WHERE user_level='1' AND user_level='0'");
-while($row = mysql_fetch_array($get)) 
-{
-	$admin = $row['user_level'];
-}
-if (isset($_SESSION['username']) && isset($_SESSION['user_level'])) {
-	$username = $_SESSION['username'];
-	if($_SESSION['user_level'] == 0) {
-	echo "<a href='login.php'>Log in</a> | <a href='logout.php'>Log out</a>
-		  <h1>Login with User Access</h1>";
-	exit();
-	} else if ($_SESSION['user_level'] == 1) {
-		echo "<a href='login.php'>Log in</a> | <a href='logout.php'>Log out</a>
-		 <h1>Login with Admin Access<h1>";
-		exit();
-	}else{
-	echo "<a href = 'manual.php'> Login was unsuccessful </a>";
-	exit();
-    }
-}
-?>*/
+
 
 <?php include_once('footer.php'); ?>
