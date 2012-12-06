100cfa266e4611885bd6a8c7ff36f5b93193b626
misoks <cathy.a.fisher@gmail.com>
2012-12-06 09:53:20 -0500

    Ignore our active db file

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = mysql_connect("localhost","kdsoltis", "sql1user") or die('Fail message');//change to your username/password to test on your machine
-mysql_select_db("itaps") or die("Fail message");//create itaps db and run createTables, loadData before using these php files
-?>
